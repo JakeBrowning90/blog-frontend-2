7456bb1b944fb717e0cbd934a661e7fcf718b27d
Jake Browning <jacobbrowning90@gmail.com>
2024-04-26 19:46:37 +0100

    Delete unused reader route and controller

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 require('dotenv').config()
 
-var LocalStorage = require('node-localstorage').LocalStorage;
+const LocalStorage = require('node-localstorage').LocalStorage;
 localStorage = new LocalStorage('./scratch');
 
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const postsRouter = require('./routes/posts');
+const commentsRouter = require('./routes/comments');
 
-var indexRouter = require('./routes/index');
-var authorsRouter = require('./routes/authors');
-var postsRouter = require('./routes/posts');
-var readersRouter = require('./routes/readers');
-var commentsRouter = require('./routes/comments');
-
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -34,9 +32,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/authors', authorsRouter);
+app.use('/users', usersRouter);
 app.use('/posts', postsRouter);
-app.use('/readers', readersRouter);
 app.use('/comments', commentsRouter);
 
 // catch 404 and forward to error handler
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-
 // Author login (YES)
 
 // Create author
