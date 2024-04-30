532ed8b90423aabf246dcf401a626fca964aa9a8
Jake Browning <jacobbrowning90@gmail.com>
2024-04-30 17:57:29 +0100

    Update api address

--- controllers/apiURLController.js
+++ controllers/apiURLController.js
@@ -1,5 +1,5 @@
-// const address = "https://jakebrowning-blog-api.fly.dev/";
-const address = "http://localhost:3000/";
+const address = "https://jakebrowning-blog-api.fly.dev/";
+// const address = "http://localhost:3000/";
 
 module.exports = {
     address: address
